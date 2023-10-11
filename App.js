3e2ad48a961590bc7e4261039d2fb698d96df27a
DahabSharaby <98348719+DahabSharaby@users.noreply.github.com>
2023-10-11 21:36:44 +0100

    added button

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const handleButtonPress = () => {
+    alert('Button Pressed');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Hello World ! </Text>
-      <Text>Dahab Sharaby </Text>
+      <Text>Hello World!</Text>
+      <Text>Dahab Sharaby</Text>
+      <Button title="Press Me" onPress={handleButtonPress} />
       <StatusBar style="auto" />
     </View>
   );
