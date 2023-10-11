af2bb0d08f9d9159d497bec37d986379782e954d
DahabSharaby <98348719+DahabSharaby@users.noreply.github.com>
2023-10-11 21:40:47 +0100

    changed location of butten

--- App.js
+++ App.js
@@ -3,14 +3,19 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   const handleButtonPress = () => {
+    // Define the action you want to perform when the button is pressed.
     alert('Button Pressed');
   };
 
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>Dahab Sharaby</Text>
-      <Button title="Press Me" onPress={handleButtonPress} />
+      <View style={styles.content}>
+        <Text>Hello World?!</Text>
+        <Text>Dahab Sharaby</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Press Me" onPress={handleButtonPress} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -20,7 +25,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  content: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    paddingBottom: 20, // You can adjust the value to control the spacing between the content and the button
+    paddingHorizontal: 20, // Optional padding for the button container
   },
 });
+
