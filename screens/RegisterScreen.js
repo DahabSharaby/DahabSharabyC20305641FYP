4d138788e6a0534065fe596773c17749ee677c61
DahabSharaby <98348719+DahabSharaby@users.noreply.github.com>
2023-10-16 17:31:07 +0100

    Web added + reg info

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -1,15 +1,46 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-
+import React, { useState } from 'react'
+import {Button, Input, Impage } from "react-native-elements";
+import {StatusBar } from "expo-status-bar";
+import { KeyboardAvoidingView } from 'react-native';
 
 const RegisterScreen = ({navigation}) => {
+
+  const [email, setEmail] = useState("")
+  const [pass, setPassword] = useState("")
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
-  )
-}
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+          <StatusBar style="light"/>
+          <Text>RegisterScreen</Text>
+          <View style= {styles.inputContainer}>
+              <Input placeholder="Email" autoFocus type="email"
+              value={email} onChangeText={(text) => setEmail(text)}/>
 
-export default RegisterScreen
+              <Input placeholder="Password" secureTextEntry type="password"
+              value={pass} onChangeText={(text) => setPassword(text)}/>
 
-const styles = StyleSheet.create({})
+          </View>
+
+          <Button onPress={() => navigation.navigate("Main")} 
+           containerStyle={styles.button} title="Login"/>
+          
+          <Button onPress={() => navigation.navigate("Register")} 
+          containerStyle={styles.button} type="outline" title="Regstier"/>
+          
+       </KeyboardAvoidingView>
+  );
+};
+
+export default RegisterScreen 
+
+const styles = StyleSheet.create({
+  Container:{
+      flex: 1,
+      alignItems: "center",
+      justifyContent:"center",
+      padding: 10,
+  },
+  inputContainer:{},
+  button:{},
+
+});
